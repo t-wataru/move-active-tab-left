90b3ac65f4f0bd31f1b0fd6f12df74b9a4496c87
Wataru Tsujimoto <tsujimoto.wataru@g.wakayama-u.jp>
2020-12-04 21:44:06 +0900

    disable moving new tab

--- background.js
+++ background.js
@@ -18,19 +18,19 @@ browser.tabs.onActivated.addListener((tab) => {
 });
 
 
-browser.tabs.onCreated.addListener((tab) => {
-  debug && console.log(tab)
-  browser.tabs.query({pinned:true, windowId:tab.windowId}).then((pinnedTabs)=>{
-  	debug && console.log(pinnedTabs);
-  	debug && console.log(pinnedTabs.length);
-    debug && console.log(pinnedTabs.map(pinnedTab=>pinnedTab.id))
-    debug && console.log(tab.id)
-    let activatedTagIsPinnedTab = pinnedTabs.map(pinnedTab=>pinnedTab.id).includes(tab.id)
-    if(activatedTagIsPinnedTab) {
-      browser.tabs.move(tab.id, {windowId: tab.windowId, index: 0});
-      debug && console.log("pinned tag activated");
-      return;
-    }
-    let moving = browser.tabs.move(tab.id, {windowId: tab.windowId, index: pinnedTabs.length});
-  });
-});
+// browser.tabs.onCreated.addListener((tab) => {
+//   debug && console.log(tab)
+//   browser.tabs.query({pinned:true, windowId:tab.windowId}).then((pinnedTabs)=>{
+//   	debug && console.log(pinnedTabs);
+//   	debug && console.log(pinnedTabs.length);
+//     debug && console.log(pinnedTabs.map(pinnedTab=>pinnedTab.id))
+//     debug && console.log(tab.id)
+//     let activatedTagIsPinnedTab = pinnedTabs.map(pinnedTab=>pinnedTab.id).includes(tab.id)
+//     if(activatedTagIsPinnedTab) {
+//       browser.tabs.move(tab.id, {windowId: tab.windowId, index: 0});
+//       debug && console.log("pinned tag activated");
+//       return;
+//     }
+//     let moving = browser.tabs.move(tab.id, {windowId: tab.windowId, index: pinnedTabs.length});
+//   });
+// });
